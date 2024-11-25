a8ecfe245510f9912084bdf5f9fc6ca5d02303cf
Alexandra <atamanenkoa@school73kharkov.com>
2024-11-25 23:17:34 +0200

    update class by logger

--- model_saver.py
+++ model_saver.py
@@ -1,28 +1,16 @@
 import joblib
-import logging
+from logger_config import get_logger
 
 
 class MovieSaver:
     def __init__(self):
-        self.logger = self.setup_logger()
+        self.logger = get_logger("MovieSaver")
 
-    @staticmethod
-    def setup_logger():
-        logger = logging.getLogger('MovieSaver')
-        logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        return logger
-
-    @staticmethod
-    def save_model(model, model_path):
+    def save_model(self, model, model_path):
         joblib.dump(model, model_path)
-        logging.info(f"Model saved to {model_path}")
+        self.logger.info(f"Model saved to {model_path}")
 
-    @staticmethod
-    def load_model(model_path):
+    def load_model(self, model_path):
         model = joblib.load(model_path)
-        logging.info(f"Model loaded from {model_path}")
+        self.logger.info(f"Model loaded from {model_path}")
         return model
